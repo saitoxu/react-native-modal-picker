37dfdd376521b5ade502d9d2b85db7c656ef4524
Yosuke Saito <yosuke.saito1202@gmail.com>
2018-06-15 01:32:14 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 
 export default class extends React.Component {
   render() {
-    return <View style={{ flex: 1, backgroundColor: 'red' }} />
+    const { containerStyle } = this.props
+    return <View style={[{ flex: 1, backgroundColor: 'red' }, containerStyle]} />
   }
 }
